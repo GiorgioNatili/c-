eb21319f0f6376dfdf02f147228a43d148f0ca1d
GiorgioNatili <g.natili@gnstudio.com>
2015-07-07 10:48:25 -0400

    Add recursion to the filesystem example

--- FileSystem/FileSystem/main.cpp
+++ FileSystem/FileSystem/main.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 
+std::string iterateDir(boost::filesystem::path currentPath, std::string &output);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Please enther the directory you want to explore...\n";
@@ -21,16 +23,42 @@ int main(int argc, const char * argv[]) {
     using namespace boost::filesystem;
     
     path currentPath = inpath;
-    directory_iterator iterator {currentPath};
     
-    while (iterator != directory_iterator{}) {
-        
-        std::cout << *iterator++ << '\n';
-        
-    }
+    std::string explodedPath;
+    iterateDir(currentPath, explodedPath);
+    
+    std::cout << explodedPath;
     
     // Ask the user if he wants to print it out to a file
     
     return 0;
+    
+}
+
+std::string iterateDir(boost::filesystem::path currentPath, std::string &output) {
+    
+    using namespace boost::filesystem;
+    
+    directory_iterator iterator {currentPath};
+    
+    while (iterator != directory_iterator{}) {
+        
+        file_status s = status(*iterator);
+        
+        // .operator is where the value of the derefenced variable is stored
+        // file_status s = status(iterator.operator*());
+        
+        if (is_directory(s)){
+            
+            iterateDir(*iterator, output);
+            
+        }
+        
+        path current = *iterator++;
+        output += current.string() + '\n';
+        
+    }
+    
+    return output;
     
 }
